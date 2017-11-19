4b21056c47c78135a35a3f0204e7812c823cfef0
marcdaniel48 <marcdaniel48@gmail.com>
2017-11-19 18:11:32 -0500

    Added dialog on back press in settings In the settings activity, when the user clicks on the back button, an alert dialog will appear asking for confirmation if the user wants to quit the activity.

--- app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/SettingsActivity.java
+++ app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/SettingsActivity.java
@@ -1,8 +1,11 @@
 package roantrevormarcdanieltiffany.com.dawsonbestfinder;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.EditText;
 
@@ -51,6 +54,32 @@ public class SettingsActivity extends Activity
         editor.putString("email", emailET.getText().toString());
         editor.putString("password", passwordET.getText().toString());
         editor.putString("datestamp", datestampET.getText().toString());
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setPositiveButton(R.string.settings_back_dialog_yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                SettingsActivity.super.onBackPressed();
+            }
+        });
+
+        builder.setNegativeButton(R.string.settings_back_dialog_no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.setMessage(R.string.settings_back_dialog_message).setTitle(R.string.settings_back_dialog_title);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
