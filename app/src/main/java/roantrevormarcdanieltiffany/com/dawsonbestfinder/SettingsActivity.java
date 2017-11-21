b46dcec8b2d603379ba66ab37ff26b50c5fc1094
marcdaniel48 <marcdaniel48@gmail.com>
2017-11-21 08:19:46 -0500

    Minor code modifications + Javadocs - Removed unused import statement - Renamed class fields from nameType to typeName - Changed the default value from null to "", when using SharedPreferences method putString - Added JavaDocs

--- app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/SettingsActivity.java
+++ app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/SettingsActivity.java
@@ -5,18 +5,26 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.EditText;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Date;
 
+/**
+ * SettingsActivity.class
+ *
+ * Class that handles the logic for the settings activity.
+ */
 public class SettingsActivity extends Activity
 {
-    EditText firstNameET, lastNameET, emailET, passwordET, datestampET;
+    EditText etFirstName, etLastName, etEmail, etPassword, etDatestamp;
 
+    /**
+     * When the activity is created, call super.onCreate, then look into SharedPreferences for saved user information.
+     * If SharedPreferences contain user information values, set those values into the activity's EditText fields.
+     *
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -24,45 +32,52 @@ public class SettingsActivity extends Activity
 
         setContentView(R.layout.activity_settings);
 
-        firstNameET = (EditText) findViewById(R.id.firstNameEditText);
-        lastNameET = (EditText) findViewById(R.id.lastNameEditText);
-        emailET = (EditText) findViewById(R.id.emailEditText);
-        passwordET = (EditText) findViewById(R.id.passwordEditText);
-        datestampET = (EditText) findViewById(R.id.datestampEditText);
+        etFirstName = (EditText) findViewById(R.id.firstNameEditText);
+        etLastName = (EditText) findViewById(R.id.lastNameEditText);
+        etEmail = (EditText) findViewById(R.id.emailEditText);
+        etPassword = (EditText) findViewById(R.id.passwordEditText);
+        etDatestamp = (EditText) findViewById(R.id.datestampEditText);
 
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
 
         if(prefs.contains("firstName"))
-            firstNameET.setText(prefs.getString("firstName", null));
+            etFirstName.setText(prefs.getString("firstName", ""));
 
         if(prefs.contains("lastName"))
-            lastNameET.setText(prefs.getString("lastName",null));
+            etLastName.setText(prefs.getString("lastName",""));
 
         if(prefs.contains("email"))
-            emailET.setText(prefs.getString("email", null));
+            etEmail.setText(prefs.getString("email", ""));
 
         if(prefs.contains("password"))
-            passwordET.setText(prefs.getString("password", null));
+            etPassword.setText(prefs.getString("password", ""));
 
         if(prefs.contains("datestamp"))
-            datestampET.setText(prefs.getString("datestamp", null));
+            etDatestamp.setText(prefs.getString("datestamp", ""));
     }
 
+    /**
+     * Takes the values found in the EditText fields and stores them in SharedPreferences.
+     * @param view
+     */
     public void saveSettings(View view)
     {
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
 
-        editor.putString("firstName", firstNameET.getText().toString());
-        editor.putString("lastName", lastNameET.getText().toString());
-        editor.putString("email", emailET.getText().toString());
-        editor.putString("password", passwordET.getText().toString());
+        editor.putString("firstName", etFirstName.getText().toString());
+        editor.putString("lastName", etLastName.getText().toString());
+        editor.putString("email", etEmail.getText().toString());
+        editor.putString("password", etPassword.getText().toString());
 
         Date date = new Date();
-        datestampET.setText(date.toString());
-        editor.putString("datestamp", datestampET.getText().toString());
+        etDatestamp.setText(date.toString());
+        editor.putString("datestamp", etDatestamp.getText().toString());
     }
 
+    /**
+     * When the back button is pressed, an alert dialog will appear asking for exit confirmation.
+     */
     @Override
     public void onBackPressed()
     {
