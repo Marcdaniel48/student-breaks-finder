e0ea479b6944901e7ff70471bee880936008c5a9
marcdaniel48 <marcdaniel48@gmail.com>
2017-11-24 08:56:40 -0500

    Added DBHelper for notes

---
+++ app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/database/NotesDBHelper.java
@@ -0,0 +1,68 @@
+package roantrevormarcdanieltiffany.com.dawsonbestfinder.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NotesDBHelper extends SQLiteOpenHelper
+{
+    public static final String TABLE_NOTES = "notes";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NOTE = "note";
+
+    private static final String DATABASE_NAME = "notes.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE = "create table " + TABLE_NOTES + "(" + COLUMN_ID + " integer primary key autoincrement, " + COLUMN_NOTE + " text not null" + ");";
+
+    private static NotesDBHelper notesDBH = null;
+
+    private NotesDBHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static NotesDBHelper getNotesDBHelper(Context context)
+    {
+        if(notesDBH == null)
+        {
+            notesDBH = new NotesDBHelper(context.getApplicationContext());
+        }
+
+        return notesDBH;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+        sqLiteDatabase.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1)
+    {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTES);
+        sqLiteDatabase.execSQL(DATABASE_CREATE);
+    }
+
+    public long insertNewNote(String note)
+    {
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_NOTE, note);
+
+        long code = getWritableDatabase().insert(TABLE_NOTES, null, cv);
+        return code;
+    }
+
+    public Cursor getNotes()
+    {
+        return getReadableDatabase().query(TABLE_NOTES, null, null, null, null, null, null);
+    }
+
+    public int deleteNote(int id)
+    {
+        return getWritableDatabase().delete(TABLE_NOTES, COLUMN_ID + "=?", new String[] {String.valueOf(id)});
+    }
+}
