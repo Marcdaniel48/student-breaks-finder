becb72a8c95c1e615d85e0a28f44423008c2acfd
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-12-01 11:59:50 -0500

    :sparkles: Parse json to Forecast objects

--- app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/api/OpenWeather.java
+++ app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/api/OpenWeather.java
@@ -13,10 +13,15 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Array;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
+
+import roantrevormarcdanieltiffany.com.dawsonbestfinder.beans.Forecast;
 
 
 /**
@@ -49,11 +54,26 @@ public class OpenWeather {
     // Response keys
     static final String VALUE_KEY = "value";
     static final String MAIN_KEY = "main";
-    static final String TEMP_KEY = "temp";
+    static final String TEMP_KEY = "temp_min";
+    static final String TEMP_MAX_KEY = "temp_max";
+    static final String TEMP_MIN_KEY = "temp_min";
+    static final String PRESSURE_KEY = "pressure";
+    static final String SEA_LEVEL_KEY = "sea_level";
+    static final String GRND_LEVEL_KEY = "grnd_level";
+    static final String HUMIDITY_KEY = "humidity";
+    static final String TEMP_KF_KEY = "temp_kf";
     static final String WEATHER_KEY = "weather";
+    static final String WEATHER_ID_KEY = "id";
+    static final String WEATHER_DESCRIPTION_KEY = "description";
+    static final String WEATHER_ICON_KEY = "icon";
     static final String CLOUDS_KEY = "clouds";
+    static final String CLOUDS_ALL_KEY = "all";
     static final String WIND_KEY = "wind";
+    static final String WIND_SPEED_KEY = "speed";
+    static final String WIND_DEG_KEY = "deg";
     static final String RAIN_KEY = "rain";
+    static final String H3_KEY = "3h";
+    static final String SNOW_KEY = "snow";
     static final String LIST_KEY = "list";
     static final String DT_TXT_KEY = "dt_txt";
 
@@ -171,7 +191,7 @@ public class OpenWeather {
         return new String[] {uviJSON.getString(VALUE_KEY)};
     }
 
-    public static String[] getForecastFromJSON(String jsonResponse) throws JSONException {
+    public static List<Forecast> getForecastFromJSON(String jsonResponse) throws JSONException {
         // Get the hour we want
         Calendar rightNow = Calendar.getInstance();
         String hour = rightNow.get(Calendar.HOUR_OF_DAY) + ":00:00";
@@ -198,25 +218,107 @@ public class OpenWeather {
         }
 
         JSONArray forecastArr = json.getJSONArray(LIST_KEY);
-
-        String[] parsedWeatherData = new String[forecastArr.length()];
+        List<Forecast> forecasts = new ArrayList<>();
 
         for (int i = 0; i < forecastArr.length(); i++) {
-            String date;
-            String highAndLow;
-
             // Get JSON object representing the day
             JSONObject dayForecast = forecastArr.getJSONObject(i);
 
             // Only take objects whose hour is the one we want
             if (dayForecast.getString(DT_TXT_KEY).matches("*"+hour+"$")) {
-                JSONObject mainObj = dayForecast.getJSONObject(MAIN_KEY);
-                JSONObject weatherObj = dayForecast.getJSONArray(WEATHER_KEY).getJSONObject(0);
-
+                Forecast forecast = getForecastFromDay(dayForecast);
+                forecasts.add(forecast);
             }
-
         }
 
-        return parsedWeatherData;
+        return forecasts;
+    }
+
+    /**
+     * Parses JSON and makes a Forecast object
+     *
+     * @param dayForecast
+     * @return
+     * @throws JSONException
+     */
+    private static Forecast getForecastFromDay(JSONObject dayForecast) throws JSONException {
+        JSONObject mainObj = dayForecast.getJSONObject(MAIN_KEY);
+        JSONObject weatherObj = dayForecast.getJSONArray(WEATHER_KEY).getJSONObject(0);
+
+        Forecast forecast = new Forecast();
+
+        if (mainObj.has(TEMP_KEY)) {
+            forecast.setTemperature(mainObj.getDouble(TEMP_KEY));
+        }
+
+        if (mainObj.has(TEMP_MIN_KEY)) {
+            forecast.setTemperatureMin(mainObj.getDouble(TEMP_MIN_KEY));
+        }
+
+        if (mainObj.has(TEMP_MAX_KEY)) {
+            forecast.setTemperatureMax(mainObj.getDouble(TEMP_MAX_KEY));
+        }
+
+        if (mainObj.has(PRESSURE_KEY)) {
+            forecast.setPressure(mainObj.getDouble(PRESSURE_KEY));
+        }
+
+        if (mainObj.has(SEA_LEVEL_KEY)) {
+            forecast.setSeaLevel(mainObj.getDouble(SEA_LEVEL_KEY));
+        }
+
+        if (mainObj.has(GRND_LEVEL_KEY)) {
+            forecast.setGrndLevel(mainObj.getDouble(GRND_LEVEL_KEY));
+        }
+
+        if (mainObj.has(HUMIDITY_KEY)) {
+            forecast.setHumidity(mainObj.getDouble(HUMIDITY_KEY));
+        }
+
+        if (mainObj.has(TEMP_KF_KEY)) {
+            forecast.setTemperatureKf(mainObj.getDouble(TEMP_KF_KEY));
+        }
+
+        if (weatherObj.has(WEATHER_ID_KEY)) {
+            forecast.setWeatherId(weatherObj.getInt(WEATHER_ID_KEY));
+        }
+
+        if (weatherObj.has(MAIN_KEY)) {
+            forecast.setWeatherMain(weatherObj.getString(MAIN_KEY));
+        }
+
+        if (weatherObj.has(WEATHER_DESCRIPTION_KEY)) {
+            forecast.setWeatherDescription(weatherObj.getString(WEATHER_DESCRIPTION_KEY));
+        }
+
+        if (weatherObj.has(WEATHER_DESCRIPTION_KEY)) {
+            forecast.setWeatherDescription(weatherObj.getString(WEATHER_DESCRIPTION_KEY));
+        }
+
+        if (weatherObj.has(WEATHER_DESCRIPTION_KEY)) {
+            forecast.setWeatherDescription(weatherObj.getString(WEATHER_DESCRIPTION_KEY));
+        }
+
+        if (dayForecast.has(CLOUDS_KEY) && dayForecast.getJSONObject(CLOUDS_KEY).has(CLOUDS_ALL_KEY)) {
+            forecast.setCloudsAllPercentage(dayForecast.getJSONObject(CLOUDS_KEY).getDouble(CLOUDS_ALL_KEY));
+        }
+
+        if (dayForecast.has(WIND_KEY)) {
+            JSONObject windObj = dayForecast.getJSONObject(WIND_KEY);
+            if (windObj.has(WIND_DEG_KEY))
+                forecast.setWindDeg(windObj.getDouble(WIND_DEG_KEY));
+            if (windObj.has(WIND_SPEED_KEY))
+                forecast.setWindSpeed(windObj.getDouble(WIND_SPEED_KEY));
+        }
+
+        if (dayForecast.has(RAIN_KEY) && dayForecast.getJSONObject(RAIN_KEY).has(H3_KEY)) {
+            forecast.setRainMM3h( dayForecast.getJSONObject(RAIN_KEY).getDouble(H3_KEY));
+        }
+
+        if (dayForecast.has(SNOW_KEY) && dayForecast.getJSONObject(SNOW_KEY).has(H3_KEY)) {
+            forecast.setSnowVol3h( dayForecast.getJSONObject(SNOW_KEY).getDouble(H3_KEY));
+        }
+
+        return forecast;
     }
 }
