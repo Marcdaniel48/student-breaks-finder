f4cac45e816a982f2dbb64970c3f13a524fbe7c3
marcdaniel48 <marcdaniel48@gmail.com>
2017-12-04 14:47:59 -0500

    Created Friend bean

---
+++ app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/beans/Friend.java
@@ -0,0 +1,57 @@
+package roantrevormarcdanieltiffany.com.dawsonbestfinder.beans;
+
+public class Friend
+{
+    private static final String TAG = Friend.class.getSimpleName();
+
+    private String email;
+    private String firstname;
+    private String lastname;
+
+    public Friend()
+    {
+        this("","","");
+    }
+
+    public Friend(String email, String firstname, String lastname)
+    {
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setFirstname(String firstname)
+    {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname()
+    {
+        return firstname;
+    }
+
+    public void setLastname(String lastname)
+    {
+        this.lastname = lastname;
+    }
+
+    public String getLastname()
+    {
+        return lastname;
+    }
+
+    public String toString()
+    {
+        return firstname + ", " + lastname + ", " + email;
+    }
+}
