65a75dae4c9c13acb3bf3f8c4a6e1c67c4e6e772
Trevor Eames <mrtvor@gmail.com>
2017-12-04 09:21:13 -0500

    finished up

--- app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/AcademicCalendarActivity.java
+++ app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/AcademicCalendarActivity.java
@@ -3,22 +3,52 @@ package roantrevormarcdanieltiffany.com.dawsonbestfinder;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 
 /**
- * Created by mrtvor on 2017-11-24.
+ * Academic Calendar activity. Has radio buttons for
+ * the season, an edit text for the year, and a
+ * submit button
+ *
+ * @author Tiffany Le-Nguyen
+ * @author Roan Chamberlain
+ * @author Marc-Daniel Dialogo
+ * @author Trevor Eames
  */
-
 public class AcademicCalendarViewActivity extends Activity {
     private final static String TAG = "FRAG-CALENDARACTIVITY";
 
     private WebView webview;
+    private RadioGroup rGroup;
+    private EditText yearText;
+
     String url = "https://www.dawsoncollege.qc.ca/registrar/";
     String fullUrl = "https://www.dawsoncollege.qc.ca/registrar/fall-2017-day-division/";
 
+    /**
+     * Will load a new url with the user input
+     */
     public void onSubmit() {
+        int rId = rGroup.getCheckedRadioButtonId();
+        View radioButton = rGroup.findViewById(rId);
+        int idx = rGroup.indexOfChild(radioButton);
 
+        RadioButton r = (RadioButton) rGroup.getChildAt(idx);
+        String text = r.getText().toString();
+
+        String year = yearText.getText().toString();
+
+        if(text == "summer") {
+            webview.loadUrl(url+text+"-"+year+"/");
+        } else {
+            webview.loadUrl(url+text+"-"+year+"-day-division/");
+        }
     }
 
     @Override
@@ -28,6 +58,9 @@ public class AcademicCalendarViewActivity extends Activity {
         Log.d(TAG, "onCreate()");
 
         webview = findViewById(R.id.webview);
+        rGroup = findViewById(R.id.seasonGroup);
+        yearText = findViewById(R.id.yearText);
+
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
