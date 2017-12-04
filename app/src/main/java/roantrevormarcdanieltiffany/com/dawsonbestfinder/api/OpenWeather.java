92cb942e649b5fae202b8661ebe2df1a43b2f4f4
Trevor Eames <mrtvor@gmail.com>
2017-12-04 04:45:59 -0500

    prepping to pull dev

--- app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/api/OpenWeather.java
+++ app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/api/OpenWeather.java
@@ -30,7 +30,6 @@ import java.util.Scanner;
 public class OpenWeather {
     private static final String TAG = OpenWeather.class.getSimpleName();
 
-    private static final String OPEN_WEATHER_UVI_URL = "http://api.openweathermap.org/data/2.5/uvi?appid=8116230ada270186d54714add001180f";
     private static final String OPEN_WEATHER_TEMP_URL = "http://api.openweathermap.org/data/2.5/weather?appid=8116230ada270186d54714add001180f";
     // The format we want our API to return
     private static final String FORMAT = "json";
@@ -42,8 +41,6 @@ public class OpenWeather {
     static final String LON_PARAM = "lon";
 
     // Response keys
-    static final String VALUE_KEY = "value";
-
     private Context context;
 
     /**
@@ -54,32 +51,6 @@ public class OpenWeather {
     public OpenWeather(Context context) {
         Log.d(TAG, "called OpenWeather()");
         this.context = context;
-    }
-
-    /**
-     * Builds the URL request for the open weather api
-     *
-     * @param lat
-     *      latitude of location
-     * @param lon
-     *      longitude of location
-     * @return The URL used to query the weather api
-     */
-    public static URL buildUrl(String lat, String lon) {
-        Log.d(TAG, "called buildUrl()");
-        Uri builtUri = Uri.parse(OPEN_WEATHER_UVI_URL).buildUpon()
-                .appendQueryParameter(LAT_PARAM, lat)
-                .appendQueryParameter(LON_PARAM, lon)
-                .build();
-
-        try {
-            URL url = new URL(builtUri.toString());
-            Log.d(TAG, "Built URI" + url);
-            return url;
-        } catch (MalformedURLException err) {
-            Log.d(TAG, err.getMessage());
-            return null;
-        }
     }
 
     /**
@@ -129,37 +100,6 @@ public class OpenWeather {
 
             return hasInput? scanner.next() : null;
         }
-    }
-
-    /**
-     * Parses JSON and returns the value for the uvi
-     * (ultraviolet index)
-     *
-     * @param context
-     * @param jsonResponse
-     *      JSON response from api
-     * @return double UVI value
-     * @throws JSONException
-     */
-    public static String[] getUviValueFromJSON(Context context, String jsonResponse) throws JSONException{
-        JSONObject uviJSON = new JSONObject(jsonResponse);
-
-        if (uviJSON.has(ERROR_MESSAGE)) {
-            int errorCode = uviJSON.getInt(ERROR_MESSAGE);
-
-            switch (errorCode) {
-                case HttpURLConnection.HTTP_OK:
-                    break;
-                case HttpURLConnection.HTTP_NOT_FOUND:
-                    Log.e(TAG, "Could not find location");
-                    break;
-                default:
-                    Log.e(TAG, "Something is wrong on their side");
-                    break;
-            }
-        }
-
-        return new String[] {uviJSON.getString(VALUE_KEY)};
     }
 
     /**
