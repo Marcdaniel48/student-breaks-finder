4a67e59d4a5924db1f8304598508e98b21158dff
Trevor Eames <mrtvor@gmail.com>
2017-12-06 15:52:07 -0500

    layout, manifest amendments, and beginning of class

---
+++ app/src/main/java/roantrevormarcdanieltiffany/com/dawsonbestfinder/WhoIsFreeActivity.java
@@ -0,0 +1,14 @@
+package roantrevormarcdanieltiffany.com.dawsonbestfinder;
+
+import android.content.Context;
+
+/**
+ * Created by mrtvor on 2017-12-06.
+ */
+
+public class WhoIsFreeActivity extends MenuActivity {
+
+    private static final String TAG = WhoIsFreeActivity.class.getSimpleName();
+    private Context context;
+    //private EditText
+}
